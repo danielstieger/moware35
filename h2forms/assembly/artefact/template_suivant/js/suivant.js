6579a2f7b1fa5dffde80847023811947738ec784
Daniel Stieger <daniel.stieger@modellwerkstatt.org>
2018-10-22 15:54:56 +0200

    suivant template initial commit

---
+++ h2forms/assembly/artefact/template_suivant/js/suivant.js
@@ -0,0 +1,24 @@
+/*
+ *	suivant - the next generation template for mde and mobile devices
+ *            used in h2H2Forms
+ *
+ *
+ *	daniel stieger,
+ *	modellwerkstatt.org
+ *
+ *
+ *  Version History:
+ *
+ *     1.0 Initial Version, Koblach Autumn 2018
+ *
+ *
+ *
+ */
+
+
+var $ = function(query) {
+  return document.querySelector(query);
+};
+var $$ = function(query) {
+  return document.querySelectorAll(query);
+};
