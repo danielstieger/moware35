ed3e70e4a5db7d8bba56cdbb7495893795f4e067
Daniel Stieger <daniel.stieger@modellwerkstatt.org>
2018-11-06 15:18:43 +0100

    problems on eb

--- h2forms/assembly/artefact/template_suivant/js/suivant.js
+++ h2forms/assembly/artefact/template_suivant/js/suivant.js
@@ -36,10 +36,15 @@ function openPage(page) {
 }
 
 function toggleDropDown(x) {
+    console.log('toggleDropDown called for ' + x);
+    var icon_i = x.parentElement.querySelector('i');
+
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
+        icon_i.innerHTML = 'close';
     } else {
         x.className = x.className.replace(" w3-show", "");
+        icon_i.innerHTML = 'more_vert';
     }
 }
 
