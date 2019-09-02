0c61e23fae0f5749ef933b2f09be55977911b31f
Daniel Stieger <daniel.stieger@modellwerkstatt.org>
2019-09-02 08:52:56 +0200

    TC25 scan fix

--- h2forms/assembly/artefact/template_suivant/js/hwsprt_zebra2_TC25.js
+++ h2forms/assembly/artefact/template_suivant/js/hwsprt_zebra2_TC25.js
@@ -59,8 +59,8 @@ function zzScanReceived(params){
 function zzScanSubmit(){
     // issuing a scan, which in turn will fire
     // the scan conclusion then ... and submit
-    svLog('zzScanSubmit', 'zebra issuing a scan');
     EB.Barcode.stop();
+    EB.Barcode.triggerType = EB.Barcode.SOFT_ONCE;
     EB.Barcode.start();
 }
 
